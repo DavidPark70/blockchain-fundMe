6f6e81735dac9601c581eccbe69e7a71f6e0162c
Yedarm Park <yedarmpark79@gmail.com>
2022-11-13 13:06:20 -0800

    first commit

---
+++ FundMe.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract FundMe {
+    uint256 public minimumUsd = 50 * 1e18;
+
+    function fund() public payable {
+        require(
+            getConversionRate(msg.value) >= minimumUsd,
+            "Didn't send enough!"
+        );
+    }
+
+    function getPrice() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        );
+        (
+            ,
+            /* uint80 roundId */
+            int256 price,
+            ,
+            ,
+
+        ) = /* uint256 startedAt */
+            /* uint256 updatedAt */
+            /* uint80 answeredInRound */
+            priceFeed.latestRoundData();
+        // ETH in terms of USD
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate(uint256 ethAmount)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        );
+        return priceFeed.version();
+    }
+
+    function getDecimals() public view returns (uint8) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        );
+        return priceFeed.decimals();
+    }
+}
