e7aa6dd96de16296123f69c42fa325ed5e5ac108
Yedarm Park <yedarmpark79@gmail.com>
2022-11-13 16:19:42 -0800

    updates

--- FundMe.sol
+++ FundMe.sol
@@ -1,58 +1,54 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
+
     uint256 public minimumUsd = 50 * 1e18;
 
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
     function fund() public payable {
+        // check if fund is at least 50 usd
         require(
-            getConversionRate(msg.value) >= minimumUsd,
+            msg.value.getConversionRate() >= minimumUsd,
             "Didn't send enough!"
         );
+        // save record of funder
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
     }
 
-    function getPrice() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(
-            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-        );
-        (
-            ,
-            /* uint80 roundId */
-            int256 price,
-            ,
-            ,
-
-        ) = /* uint256 startedAt */
-            /* uint256 updatedAt */
-            /* uint80 answeredInRound */
-            priceFeed.latestRoundData();
-        // ETH in terms of USD
-        return uint256(price * 1e10);
+    function withdraw() public onlyOwner {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            // reset all funds to 0
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        // reset the funders array
+        funders = new address[](0);
+        // transfer funds to wallet
+        (bool callSuccess, ) = /* bytes memory dataReturned */
+        payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
     }
 
-    function getConversionRate(uint256 ethAmount)
-        public
-        view
-        returns (uint256)
-    {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
-    }
-
-    function getVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(
-            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-        );
-        return priceFeed.version();
-    }
-
-    function getDecimals() public view returns (uint8) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(
-            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-        );
-        return priceFeed.decimals();
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Sender is not owner!");
+        // proceed with the rest of the code flow
+        _;
     }
 }
---
+++ PriceConverter.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    function getPrice() internal view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        );
+        (
+            ,
+            /* uint80 roundId */
+            int256 price,
+            ,
+            ,
+
+        ) = /* uint256 startedAt */
+            /* uint256 updatedAt */
+            /* uint80 answeredInRound */
+            priceFeed.latestRoundData();
+
+        // ETH in terms of USD
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate(uint256 ethAmount)
+        internal
+        view
+        returns (uint256)
+    {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+
+    function getVersion() internal view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        );
+        return priceFeed.version();
+    }
+
+    function getDecimals() internal view returns (uint8) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        );
+        return priceFeed.decimals();
+    }
+}
